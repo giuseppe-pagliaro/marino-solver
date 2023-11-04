e0090c28cf71fa959fc51432360a1e931745cc7f
Giuseppe Pagliaro <89086449+giuseppe-pagliaro@users.noreply.github.com>
2023-11-04 09:59:16 +0100

    Delete src directory

--- src/main/errors/IncorrectExpressionSyntaxException.java
+++
@@ -1,7 +0,0 @@
-package src.main.errors;
-
-public class IncorrectExpressionSyntaxException extends RuntimeException {
-    IncorrectExpressionSyntaxException(String errorMgs) {
-        super(errorMgs);
-    }
-}
